cf9cfcfee0af27c88431bacf031bfab31e967743
ViniciusGR797 <loritoamarelo@outlook.com>
2023-01-25 08:51:21 -0300

    rotas do costumer

---
+++ src/router/rotas/costumer.go
@@ -0,0 +1,42 @@
+package rotas
+
+import "net/http"
+
+var routerCostumer = []Routes{
+	{
+		URI:             "/costumer",
+		Method:          http.MethodGet,
+		Function:        controllers.GetCostumers,
+		IsAuthenticated: false,
+	},
+	/*{
+		URI:             "/costumer/{NAME}",
+		Method:          http.MethodGet,
+		Function:        controllers.GetCostumerByName,
+		IsAuthenticated: false,
+	},
+	{
+		URI:             "/costumer/{ID}",
+		Method:          http.MethodGet,
+		Function:        controllers.GetCostumerByID,
+		IsAuthenticated: false,
+	},
+	{
+		URI:             "/costumer",
+		Method:          http.MethodPost,
+		Function:        controllers.CreateCostumer,
+		IsAuthenticated: false,
+	},
+	{
+		URI:             "/costumer/{ID}",
+		Method:          http.MethodPut,
+		Function:        controllers.UpdateCostumer,
+		IsAuthenticated: false,
+	},
+	{
+		URI:             "/costumer/{ID}",
+		Method:          http.MethodDelete,
+		Function:        controllers.DeleteCostumer,
+		IsAuthenticated: false,
+	},*/
+}
