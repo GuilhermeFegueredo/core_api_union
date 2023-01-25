3c0da116a5f6637bed600cc601e7e48641652f9c
erich-ika <erich.sabe.tudo@gmail.com>
2023-01-25 15:07:09 -0300

    Adicionado pacote bcrypt
    Incluindo duas funções simples que são base do pacote de segurança.

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
+	golang.org/x/crypto v0.5.0
 )
---
+++ src/security/security.go
@@ -0,0 +1,13 @@
+package security
+
+import "golang.org/x/crypto/bcrypt"
+
+// Hash recebe uma senha e retorna um hash dela
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+// VerificarSenha valida se o hash condiz com a senha
+func VerificarSenha(senhaComHash, senha string) error {
+	return bcrypt.CompareHashAndPassword([]byte(senhaComHash), []byte(senha))
+}
